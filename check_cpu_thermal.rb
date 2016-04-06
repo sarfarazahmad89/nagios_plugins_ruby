d5a27858e7b18346fadffaca44ed18a8f44c56ab
sarfarazahmad89 <ahmad.sarfaraz1989@gmail.com>
2016-04-06 09:53:23 +0530

    Added mce-inject templates

--- check_cpu_thermal.rb
+++ check_cpu_thermal.rb
@@ -5,8 +5,7 @@ require_relative "include/kmsgfilter.rb"
 ERRMSG="[WARNING]  CPU Temperature exceeds safe thresholds limit. Kernel error messages are displayed below\n"
 $ERR_EXIT=0 ## CPU Temperature errors are classified as warning"
 OKMSG="[OK] CPU Temperature within safe limits"
-#KMSG=`dmesg`
-KMSG=`cat working_dir/dummydmesg`
+KMSG=`dmesg`
 REGEX=[".*CPU[0-9].*temperature above.*"]
 $ERR=0
 $ERRLOG=[]
@@ -14,11 +13,11 @@ $ERRLOG=[]
 
 kmsg_filter
 if $ERR == 0
-puts "#{OKMSG}"
+ puts "#{OKMSG}"
 else
-puts "#{ERRMSG}"
-puts $ERRLOG
-ERR_EXIT=1
+ puts "#{ERRMSG}"
+ puts $ERRLOG
+ ERR_EXIT=1
 exit ERR_EXIT
 end
 
--- check_sata_errors.rb
+++ check_sata_errors.rb
@@ -6,8 +6,7 @@ require_relative "include/kernelmessage.rb"
 ERRMSG="[CRITICAL] SATA bus errors detected ! Please check SATA connections and HDD health(S.M.A.R.T).
 Kernel error messages are displayed below. For more detailed information check /var/log/dmesg"
 OKMSG="[OK] No Sata Bus Errors detected."
-#KMSG=`dmesg`
-KMSG=`cat working_dir/dummydmesg2`
+KMSG=`dmesg`
 $ERR_EXIT=0 
 REGEX=[ "\\[ .*ata[0-9].*DRDY.*",   "\\[ .*Emask.*ATA.*",".*ata[0-9].*SErr.*",   "\\[ .*ata[0-9].*error.*", "\\[ .*ata[0-9].*failed\scommand.*" ]
 $ERR=0
@@ -17,20 +16,19 @@ ERRLOG_KMSG=[]
 
 kmsg_filter
 if $ERR == 0
-puts "#{OKMSG}"
+ puts "#{OKMSG}"
 else
-
-$ERRLOG.each do |x| 
-tmp_kmsg=Kernelmessage.new
-tmp_kmsg.update(x)
-ERRLOG_KMSG << tmp_kmsg
+ $ERRLOG.each do |x| 
+ tmp_kmsg=Kernelmessage.new
+ tmp_kmsg.update(x)
+ ERRLOG_KMSG << tmp_kmsg
 end
 
 ERRLOG_KMSG.sort! {|a,b| a.timestamp <=> b.timestamp }
 
 ERRLOG_KMSG.each do |y|
-puts "[#{y.timestamp}] #{y.message}"
-end
+  puts "[#{y.timestamp}] #{y.message}"
+ end
 
 ERR_EXIT=2  ## SATA Bus errors are always critical
 exit ERR_EXIT
